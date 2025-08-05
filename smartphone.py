3bac3f9db19d9d5aaaeedf09a64e63eeb91e336d
Jorimam <iamgodwinjoewin@gmail.com>
2025-08-05 12:46:57 +0100

    samartphone commit

---
+++ smartphone.py
@@ -0,0 +1,13 @@
+print("Enter budget\n")
+budget = float(input())
+if budget >= 500:
+    if budget >= 1000:
+        print("Google Pixel 9pro recommended")
+    else:
+        print("Iphone recommended")
+else:
+    if budget >= 200:
+        print("Redmi recommende")
+    else:
+        print("Save more")
+
