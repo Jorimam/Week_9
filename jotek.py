eae2c7d75bea9c8899b9e8fdb4c818c44f0813c6
Jorimam <iamgodwinjoewin@gmail.com>
2025-08-08 10:39:47 +0100

    Adding confirm password

--- jotek.py
+++ jotek.py
@@ -36,37 +36,40 @@ elif action == "r":
 		print("Choose login option or choose another user name")
 	else:
 		password = input("Create password\n")
-		available_balance = float(input("Enter availavle balance\n"))
-		user = dict(
-		username = user_name,
-		password = password,
-		available_balance = available_balance
-		)
-		user_db.update({user_name:user})
-		user["is_verified"] = False
-		print("User created successfully")
-		print(user)
-		print("Verification will cost 1500")
-		verify = str(input("Do you wish to verify? yes or no\n"))
-		if verify == "yes":
-			if user["available_balance"] >= verification_amount:
-				user["available_balance"] -= 1500
-				user["is_verified"] = True
+		confirm_password = input("Confirm password\n")
+		if confirm_password == password:
+			available_balance = float(input("Enter availavle balance\n"))
+			user = dict(
+				username = user_name,
+				password = password,
+				available_balance = available_balance
+				)
+			user_db.update({user_name:user})
+			user["is_verified"] = False
+			print("User created successfully")
+			print(user)
+			print("Verification will cost 1500")
+			verify = str(input("Do you wish to verify? yes or no\n"))
+			if verify == "yes":
+				if user["available_balance"] >= verification_amount:
+					user["available_balance"] -= 1500
+					user["is_verified"] = True
+					print("Login successful")
+					print(user)
+					print(user_db)
+				else:
+					 print(f"Verfication costs: {verification_amount}")
+					 print(f"You cannot be verified due to Insufficeint funds: {available_balance}")
+					 print("Login successful")
+					 print(user_db)
+			elif verify == "no":
 				print("Login successful")
 				print(user)
 				print(user_db)
 			else:
-				 print(f"Verfication costs: {verification_amount}")
-				 print(f"You cannot be verified due to Insufficeint funds: {available_balance}")
-				 print("Login successful")
-				 print(user_db)
-		elif verify == "no":
-			print("Login successful")
-			print(user)
-			print(user_db)
+				print("Invalid response")
 		else:
-			print("Invalid response")
-		
+			print("Password missmatch")
 	
 else:
 	print("Error!")
